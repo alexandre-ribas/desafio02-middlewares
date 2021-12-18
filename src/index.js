591f535db2176a18c903633cf01d40c1e5f8f97c
Alexandre Ribas <alexandre.ribas@qriar.com>
2021-12-18 02:24:20 -0300

    middlewares desenvolvidos

--- src/index.js
+++ src/index.js
@@ -10,19 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username );
+
+  if(!user) {
+    return response.status(404).json({error: "User account not exists!"})
+  }
+
+  request.user = user;
+  
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) {
+    return next();
+  } else if (!user.pro && user.todos.length < 10 ) {
+    return next();
+  } else {
+    return response.status(403).json("User already have 10 todos");
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const checkUuidIsValid = validate(id);
+  if (!checkUuidIsValid) {
+    return response.status(400).json({ error: 'Id invalid' })
+  }
+
+  const user = users.find(user => user.username === username );
+
+  if(!user) {
+    return response.status(404).json({error: "User account not exists!"})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id );
+
+  if(!todo) {
+    return response.status(404).json({error: "TODO isnt from this user"})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id );
+
+  if(!user) {
+    return response.status(404).json({error: "User account not exists!"})
+  }
+
+  request.user = user;
+  
+  return next();
 }
 
 app.post('/users', (request, response) => {
